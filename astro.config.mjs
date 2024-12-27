7b2606ab379add3589d50b3f812cb4955a82bb00
DucksaberVN <business@ducksabervn.com>
2024-12-27 16:46:24 +0700

    kms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: netlify(),
 });
